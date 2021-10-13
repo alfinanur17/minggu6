49a52ea51679ae93758011b05b41625965797a8e
alfinanur17 <alfinanur0117@gmail.com>
2021-10-13 19:47:59 +0700

    pembuatan table students

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 
 class StudentSeeder extends Seeder
@@ -12,25 +13,29 @@ class StudentSeeder extends Seeder
      *
      * @return void
      */
-    public function up()
+    public function run()
     {
         DB::table('students')->insert([
             'nim' => '2031710064',
             'name' => 'Alfina Nur A',
             'class' => 'MI 2E',
             'department' => 'MI',
+            'phone_number' => '082154376954',
         ]);
         DB::table('students')->insert([
             'nim' => '2031710035',
             'name' => 'Alvira Dewi Septyan',
             'class' => 'MI 2E',
             'department' => 'MI',
+            'phone_number' => '085754376742',
         ]);
         DB::table('students')->insert([
             'nim' => '2031710118',
             'name' => 'Arifatun Nisa',
             'class' => 'MI 2E',
             'department' => 'MI',
+            'phone_number' => '089654376532',
         ]);
+
     }
 }
