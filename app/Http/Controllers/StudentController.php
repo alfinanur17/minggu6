93246b484f98189cbd6c164c6c4357bbd5eb1cb3
alfinanur17 <alfinanur0117@gmail.com>
2021-10-29 20:47:48 +0700

    Tambah relasi kelas-student

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,19 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+use App\Models\Kelas;
+
 class StudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $students = Student::all();
-        return view('students.index',['student'=>$students]);
+        $student = Student::with('kelas')->get();
+        return view('students.index', ['student'=>$student]);
     }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -24,7 +20,8 @@ class StudentController extends Controller
      */
     public function create()
     {
-        return view('students.create');
+        $kelas = Kelas::all();
+return view('students.create',['kelas'=>$kelas]);
     }
 
     /**
@@ -35,11 +32,20 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //add data
-        Student::create($request->all());
-        // if true, redirect to index
+        $student = new Student;
+        $student->nim = $request->nim;
+        $student->name = $request->name;
+        $student->department = $request->department;
+        $student->phone_number = $request->phone_number;
+        $kelas = new Kelas;
+        $kelas->id = $request->Kelas;
+        $student->kelas()->associate($kelas);
+        $student->save();
+ 
+         // if true, redirect to index
         return redirect()->route('students.index')
-        ->with('success', 'Add data success!');
+            ->with('success', 'Add data success!');
+
 
     }
 
@@ -53,6 +59,7 @@ class StudentController extends Controller
     {
         $student = Student::find($id);
         return view('students.show',['student'=>$student]);
+
     }
 
     /**
@@ -64,7 +71,9 @@ class StudentController extends Controller
     public function edit($id)
     {
         $student = Student::find($id);
-        return view('students.edit',['student'=>$student]);
+        $kelas = Kelas::all();
+        return view('students.edit',['student'=>$student, 
+    'kelas'=>$kelas]);
 
     }
 
@@ -80,10 +89,15 @@ class StudentController extends Controller
         $student = Student::find($id);
         $student->nim = $request->nim;
         $student->name = $request->name;
-        $student->class = $request->class;
         $student->department = $request->department;
         $student->phone_number = $request->phone_number;
+
+        $kelas = new Kelas;
+        $kelas->id = $request->Kelas;
+
+        $student->kelas()->associate($kelas);
         $student->save();
+        
         return redirect()->route('students.index');
     }
 
